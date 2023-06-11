133f57bc487ae34799f97a2141638ce228ffd03a
xkoren10 <xkoren10@stud.fit.vutbr.cz>
2023-06-11 23:38:51 +0200

    jsonify and readme

--- README.md
+++ README.md
@@ -1,15 +1,53 @@
-# Assignment 1
+# Python-based micorservice - Assignment 1
 ## Matej Koreň, 7.6.2023
 
-<h1> Header </h1>
+<h1> Task </h1>
+Create a simple REST API using the Flask framework in Python with following endpoints:
+
+1. GET /movies - returns a list of all the movies in the database,
+2. GET /movies/\<int:id\> return the details of a movie with the given id,
+3. POST /movies - creating a new movie in the database,
+4. PUT /movies/\<int:id\> - updates a movie with the given id.
 
 
+<h2> Solution </h2>
+<h3> Data storage </h3>
 
+To store data permanently, sqlite3 database (which is included with Python) was used. It's initialization
+script can be seen in the included file *db_init.py*, containing table creation and value constraints for 
+each column. In the *app.py* script this database is accessed via sqlite3 library and it's functions.
+To read or write into it is possible to use standard SQL queries. 
 
-<h2> Commands </h2>
+<h3> Application </h3>
 
-> curl -X POST -H "Content-type: application/json" -d "{\"title\":\"Shrek\",\"description\":\"Shrek, an ogre, embarks on a journey with a donkey to rescue Princess Fiona...\",\"release_year\":\"2001\"}" "localhost:5000/movies"
+In the solution, two app routes are used - **/movies** for 1. and 3. endpoint and **/movies/\<int:id>** 
+for 2. and 4. endpoint. Depending on the HTTP request type, different actions are executed above the database
+and proper data are stored or returned. Each method is also documented and described within the code.
+To achieve desired output, data serialization with *json.dump* library was necessary.
 
-> curl -X PUT -H "Content-type: application/json" -d "{\"title\":\"Shrek\",\"description\":\"Shrek, an ogre, embarks on a
+<h2> Testing </h2>
+
+To use the application, only Flask is needed. To install it, simply use:
+>pip install flask
+
+command or download it to your favourite IDE. After this, use:
+
+>python app.py
+
+(or run it in the editor) which should build the app and run it on the loopback adress
+(http://127.0.0.1:5000 or http://localhost:5000)
+
+One of the possibilities to test the endpoints is to use **curl** utility. 
+
+For each endpoints the command would look like this:
+1. > curl "localhost:5000/movies"
+2. > curl "localhost:5000/movies/1"
+3. > curl -X POST -H "Content-type: application/json" -d "{\"title\":\"Shrek\",\"description\":\"Shrek, an ogre, embarks on a journey with a donkey to rescue Princess Fiona...\",\"release_year\":\"2001\"}" "localhost:5000/movies"
+4. > curl -X PUT -H "Content-type: application/json" -d "{\"title\":\"Shrek\",\"description\":\"Shrek, an ogre, embarks on a
  journey with a donkey to rescue Princess Fiona...\",\"release_year\":\"2023\"}" "localhost:5000/movies/10"
+
+**NOTES**   
+Simple **GET** requests can be generated in the browser. **curl** also supports pipelining,
+therefore it accepts a JSON file in the *-d* argument, such as
+> curl ... -d @new_movie.json ...
 
--- app.py
+++ app.py
@@ -12,12 +12,15 @@ def welcome():
 
 @app.route('/movies', methods=["GET", "POST"])
 def get_or_add_movies():
+    """ First and third task """
     if request.method == 'GET':
 
         cur = g.db.cursor()
-        cur.execute("SELECT * FROM MOVIES")
-        movies = cur.fetchall()
-        return jsonify(movies)
+        cur.execute("SELECT * FROM MOVIES ORDER BY id")
+        rows = cur.fetchall()
+
+        json_output = json.dumps(serialize_data(rows), sort_keys=False)
+        return Response(json_output, mimetype='application/json')
 
     elif request.method == 'POST':
 
@@ -34,8 +37,9 @@ def get_or_add_movies():
             abort(404)
 
         else:
+            """ Data insertion """
             g.db.execute('INSERT INTO MOVIES (TITLE,DESCRIPTION,RELEASE_YEAR) VALUES (?, ?, ?)',
-                        (title, description, release_year))
+                         (title, description, release_year))
             g.db.commit()
 
             return Response('', 200)
@@ -44,16 +48,19 @@ def get_or_add_movies():
 @app.route('/movies/<int:id>', methods=["GET", "PUT"])
 def get_or_put_movie(id):
 
+    """ Second and fourth task """
     if request.method == 'GET':
+        """ Movie selection """
         cur = g.db.cursor()
         cur.execute("SELECT * FROM MOVIES WHERE id = ?", (id,))
-        movie = cur.fetchone()
+        row = cur.fetchall()
 
         """ Wrong index """
-        if movie is None:
+        if len(row) == 0:
             abort(404)
         else:
-            return jsonify(movie)
+            json_output = json.dumps(serialize_data(row), sort_keys=False)
+            return Response(json_output, mimetype='application/json')
 
     elif request.method == 'PUT':
 
@@ -63,12 +70,13 @@ def get_or_put_movie(id):
         description = data['description']
         release_year = data['release_year']
 
+        """ Movie selection"""
         cur = g.db.cursor()
         cur.execute("SELECT * FROM MOVIES WHERE id = ?", (id,))
         movie = cur.fetchone()
 
         """ Wrong index """
-        if movie is None:
+        if len(movie) == 0:
             abort(404)
         else:
 
@@ -78,22 +86,41 @@ def get_or_put_movie(id):
             g.db.commit()
             return Response('', 200)
 
+
 @app.before_request
 def before_request():
+    """ Connect to DB before calling a method"""
     g.db = sqlite3.connect('test.db')
+
 
 @app.teardown_request
 def teardown_request(exception):
+    """ Close DB connection after teardown"""
     if hasattr(g, 'db'):
         g.db.close()
 
-def get_json_data_safely(request):
+
+def get_json_data_safely(req):
     """ Accepting json or text-plain"""
-    if request.is_json:
-        data = request.json
+    if req.is_json:
+        data = req.json
     else:
-        data = json.loads(request.data)
+        data = json.loads(req.data)
     return data
+
+
+def serialize_data(data):
+    """ For prettier visualisation"""
+    serialized_movies = []
+    for row in data:
+        movie = {
+            'id': row[0],
+            'title': row[1],
+            'description': row[2],
+            'release_year': row[3]
+        }
+        serialized_movies.extend([movie])
+    return serialized_movies
 
 
 if __name__ == '__main__':
